5d171a6075bf24af9f54701e5712253542000415
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-28 22:55:12 +0530

    add: get started

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,10 +1,11 @@
-"use client";
+"use server";
 
 import { Button } from "@/components/ui/button";
-import { SignInButton } from "@clerk/nextjs";
+import { SignInButton, auth } from "@clerk/nextjs";
 import Image from "next/image";
 
 const LandingPage = () => {
+  const { userId } = auth();
   return (
     <div className="flex justify-between gap-y-4 md:px-32 px-16 py-40 relative">
       <div className="max-w-3xl space-y-5 flex-col  justify-center">
@@ -13,13 +14,21 @@ const LandingPage = () => {
           Discover stories, thinking, and expertise <br />
           from writers on any topic
         </h2>
-        <div>
-          <SignInButton mode="modal">
+        {userId ? (
+          <div>
             <Button size="lg" className="mt-2 text-lg">
               Start reading
             </Button>
-          </SignInButton>
-        </div>
+          </div>
+        ) : (
+          <div>
+            <SignInButton mode="modal">
+              <Button size="lg" className="mt-2 text-lg">
+                Get started
+              </Button>
+            </SignInButton>
+          </div>
+        )}
       </div>
       <div className="flex items-start justify-start absolute top-20 right-44">
         <Image src="/text.svg" height="450" width="450" alt="text" />
