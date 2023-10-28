481a7d6c4160e209998cfa73f137299f25fabda3
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-28 16:04:29 +0530

    add: navbar component and clerk sign in and ouut

--- app/(landing)/(components)/navbar.tsx
+++ app/(landing)/(components)/navbar.tsx
@@ -1,17 +1,27 @@
-"use client";
+"use server";
 
 import { Button } from "@/components/ui/button";
-import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, SignUpButton, UserButton, auth } from "@clerk/nextjs";
 
 export const LandingNavbar = () => {
+  const { userId } = auth();
   return (
     <>
-      <SignUpButton mode="modal">
-        <Button size="sm">Sign up</Button>
-      </SignUpButton>
-      <SignInButton mode="modal">
-        <Button size="sm">Sign out</Button>
-      </SignInButton>
+      {userId ? (
+        <div className="flex items-center justify-end">
+          <Button variant="ghost">Enter Bob</Button>
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      ) : (
+        <div className="flex items-center justify-end">
+          <SignUpButton mode="modal">
+            <Button size="sm">Sign up</Button>
+          </SignUpButton>
+          <SignInButton mode="modal">
+            <Button size="sm">Sign in</Button>
+          </SignInButton>
+        </div>
+      )}
     </>
   );
 };
