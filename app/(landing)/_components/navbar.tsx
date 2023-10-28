546b12fd21fd34e3709613df7995491bae76008f
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-28 23:28:39 +0530

    landing page styling fix

--- app/(landing)/_components/navbar.tsx
+++ app/(landing)/_components/navbar.tsx
@@ -9,7 +9,7 @@ export const LandingNavbar = () => {
   return (
     <div className="w-full flex fixed top-0 bg-background items-center p-6 md:px-32 px-16 border-b border-b-black">
       <Logo />
-      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+      <div className="md:ml-auto md:justify-end justify-end  w-full flex items-center gap-x-2">
         {userId ? (
           <>
             <Button variant="ghost">Enter Bob</Button>
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -9,8 +9,8 @@ const LandingPage = () => {
   return (
     <div className="flex justify-between gap-y-4 md:px-32 px-16 py-40 relative">
       <div className="max-w-3xl space-y-5 flex-col  justify-center">
-        <h1 className="font-bold md:text-8xl sm:text-5xl text-3xl pb-1">Stay curious.</h1>
-        <h2 className="text-base sm:text-xl md:text-2xl font-medium">
+        <h1 className="font-bold md:text-8xl sm:text-6xl text-7xl pb-1">Stay curious.</h1>
+        <h2 className="sm:text-xl md:text-2xl text-xl font-medium">
           Discover stories, thinking, and expertise <br />
           from writers on any topic
         </h2>
@@ -31,7 +31,7 @@ const LandingPage = () => {
         )}
       </div>
       <div className="flex items-start justify-start absolute top-20 right-44">
-        <Image src="/text.svg" height="450" width="450" alt="text" />
+        <Image src="/text.svg" height="450" width="450" alt="text" className="hidden lg:block" />
       </div>
     </div>
   );
