8e8a6fbe71c6e87efad356d896cc80877c911264
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-22 23:44:22 +0530

    themeprovider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { EdgeStoreProvider } from "@/lib/edgestore";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,9 +16,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en">
-        <body className={inter.className}>
-          <EdgeStoreProvider>{children}</EdgeStoreProvider>
-        </body>
+        <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
+          <body className={inter.className}>
+            <EdgeStoreProvider>{children}</EdgeStoreProvider>
+          </body>
+        </ThemeProvider>
       </html>
     </ClerkProvider>
   );
