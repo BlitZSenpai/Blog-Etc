bc119c77ffef23e88f3788d3a8019826d33115d3
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-23 20:17:27 +0530

    add: form

--- components/formcomponent.tsx
+++ components/formcomponent.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { useForm } from "react-hook-form";
-import { Form, FormField } from "./ui/form";
+import { Form, FormControl, FormField, FormItem } from "./ui/form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
 
 const formSchema = z.object({
   title: z.string().min(1).max(50),
@@ -21,11 +23,45 @@ export const FormComponent = () => {
     },
   });
 
+  const onSubmit = () => {};
+
   return (
     <Form {...form}>
-      {/* <form onSubmit={form.handleSubmit()} className="space-y-8">
-        <FormField></FormField>
-      </form> */}
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="Title" className="border-none" {...field} autoFocus />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="summary"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Textarea className="resize-none" placeholder="Summarize your story" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Textarea className="resize-none" placeholder="Tell your story..." {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
     </Form>
   );
 };
