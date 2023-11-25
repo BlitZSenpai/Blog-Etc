98085e86391cec852ac053b0521bd282c5a88dae
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-25 23:57:45 +0530

    updated post button

--- app/(home)/(routes)/home/_components/postbutton.tsx
+++ app/(home)/(routes)/home/_components/postbutton.tsx
@@ -1,16 +1,13 @@
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { ArrowRight } from "lucide-react";
 
 export const PostButton = () => {
   return (
-    <div className="flex mr-4">
-      <Link href="/home">
-        <Button size="sm" className="pl-4 border-black" variant="outline">
-          <p>Post</p>
-          <ArrowRight className="w-4 h-4 ml-0.5" />
-        </Button>
-      </Link>
-    </div>
+    <>
+      <Button type="submit" size="sm" className="pl-4 border-black" variant="outline">
+        <p>Post</p>
+        <ArrowRight className="w-4 h-4 ml-0.5" />
+      </Button>
+    </>
   );
 };
