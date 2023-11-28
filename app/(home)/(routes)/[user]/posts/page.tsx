0b6778dfa56302761e8034b9c2498fe1e0cb1082
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-29 01:38:46 +0530

    add: post card p-1

--- app/(home)/(routes)/[user]/posts/page.tsx
+++ app/(home)/(routes)/[user]/posts/page.tsx
@@ -1,15 +1,26 @@
 import { PostCard } from "@/components/postcard";
 import { userPosts } from "@/lib/actions/userposts";
+import { currentUser } from "@clerk/nextjs";
 
 interface YourPostsPageProps {
   params: { user: string };
 }
 
 const YourPostsPage = async ({ params }: YourPostsPageProps) => {
+  const user = await currentUser();
   const posts = await userPosts({ username: params.user });
 
   const post = posts.map((post) => (
-    <PostCard key={post.id} title={post.title} description={post.description} summary={post.summary} />
+    <PostCard
+      imageUrl={user?.imageUrl!}
+      username={post.username}
+      createdAt={post.createdAt}
+      key={post.id}
+      id={post.id}
+      title={post.title}
+      description={post.description}
+      summary={post.summary}
+    />
   ));
 
   return <div>{post}</div>;
--- components/postcard.tsx
+++ components/postcard.tsx
@@ -1,27 +1,40 @@
 import { Posts } from "@prisma/client";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
-
-interface PostProps {
-  title: string;
-  summary?: string;
-  description: string;
-}
+import Image from "next/image";
+import Link from "next/link";
+import { Avatar } from "./ui/avatar";
+import { format } from "date-fns";
 
 export const PostCard = ({
   title,
   summary,
   description,
-}: Pick<Posts, "summary" | "description" | "title">) => {
+  id,
+  username,
+  createdAt,
+  imageUrl,
+}: Pick<Posts, "summary" | "description" | "title" | "id" | "username" | "createdAt"> & {
+  imageUrl: string;
+}) => {
   //   const description1 = JSON.parse(description);
   return (
-    <Card className="max-w-2xl">
-      <CardHeader>
-        <CardTitle>{title}</CardTitle>
-        <CardDescription>{summary}</CardDescription>
-      </CardHeader>
-      <CardContent className="flex flex-col items-center justify-center">
-        <p>{description}</p>
-      </CardContent>
-    </Card>
+    <>
+      <Link href={`/post/${id}`}>
+        <div className="flex flex-col max-w-2xl gap-y-3 h-full border-2 bg-background">
+          <div className="flex flex-row gap-x-1 items-center">
+            <Avatar className="h-8 w-8">
+              {imageUrl ? (
+                <Image src={imageUrl} alt="profile picture" fill referrerPolicy="no-referrer" />
+              ) : null}
+            </Avatar>
+            <h3 className="text-lg">{username} · </h3>
+            <span className="text-zinc-600 text-sm"> {format(new Date(createdAt), "MMM dd")}</span>
+          </div>
+          <div className="">
+            <h1 className="font-bold text-xl mb-2">{title}</h1>
+            <p>{summary}</p>
+          </div>
+        </div>
+      </Link>
+    </>
   );
 };
