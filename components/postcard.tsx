703295db0f51f063bd86d78de0a2134136683970
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-29 20:36:28 +0530

    ADD: postcard component

--- components/postcard.tsx
+++ components/postcard.tsx
@@ -17,24 +17,22 @@ export const PostCard = ({
 }) => {
   //   const description1 = JSON.parse(description);
   return (
-    <>
+    <div className="p-3 flex flex-col max-w-2xl max-h-32 mt-2 border-b-2 bg-background">
       <Link href={`/post/${id}`}>
-        <div className="flex flex-col max-w-2xl gap-y-3 h-full border-2 bg-background">
-          <div className="flex flex-row gap-x-1 items-center">
-            <Avatar className="h-8 w-8">
-              {imageUrl ? (
-                <Image src={imageUrl} alt="profile picture" fill referrerPolicy="no-referrer" />
-              ) : null}
-            </Avatar>
-            <h3 className="text-lg">{username} · </h3>
-            <span className="text-zinc-600 text-sm"> {format(new Date(createdAt), "MMM dd")}</span>
-          </div>
-          <div className="">
-            <h1 className="font-bold text-xl mb-2">{title}</h1>
-            <p>{summary}</p>
-          </div>
+        <div className="flex flex-row gap-2 mb-2  items-center">
+          <Avatar className="h-8 w-8">
+            {imageUrl ? (
+              <Image src={imageUrl} alt="profile picture" fill referrerPolicy="no-referrer" />
+            ) : null}
+          </Avatar>
+          <h3 className="text-lg">{username} · </h3>
+          <span className="text-zinc-600 text-sm"> {format(new Date(createdAt), "MMM dd")}</span>
+        </div>
+        <div className="pb-2 pl-1">
+          <h1 className="font-bold text-xl mb-2">{title}</h1>
+          <p className="truncate">{summary}</p>
         </div>
       </Link>
-    </>
+    </div>
   );
 };
