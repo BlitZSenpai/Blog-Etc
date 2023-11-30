9e4d7b585112f65756e14c48aa9a2604ec11bb88
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-30 21:39:11 +0530

    add: card component

--- app/(home)/(routes)/post/[postId]/page.tsx
+++ app/(home)/(routes)/post/[postId]/page.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { db } from "@/lib/prismadb";
 
 interface PostPageProps {
@@ -11,7 +12,17 @@ const PostPage = async ({ params }: PostPageProps) => {
     },
   });
 
-  return <div>{post?.title}</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-4xl font-bold">{post?.title}</CardTitle>
+        <CardDescription className="text-xl">{post?.summary}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>{post?.description}</CardDescription>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default PostPage;
