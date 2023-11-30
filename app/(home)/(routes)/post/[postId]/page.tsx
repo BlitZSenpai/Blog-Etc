136f5713d1a161f9091c5502a3ccd2b8de696df1
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-30 20:16:11 +0530

    add: fetching post data

--- app/(home)/(routes)/post/[postId]/page.tsx
+++ app/(home)/(routes)/post/[postId]/page.tsx
@@ -1,5 +1,17 @@
-const PostPage = () => {
-  return <div>PostPage</div>;
+import { db } from "@/lib/prismadb";
+
+interface PostPageProps {
+  params: { postId: string };
+}
+
+const PostPage = async ({ params }: PostPageProps) => {
+  const post = await db.posts.findFirst({
+    where: {
+      id: params.postId,
+    },
+  });
+
+  return <div>{post?.title}</div>;
 };
 
 export default PostPage;
