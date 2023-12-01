5e772c01d69bcbba7bff173b210dc0f0df44eb60
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-01 23:02:53 +0530

    fix: hydration error

--- components/editor.tsx
+++ components/editor.tsx
@@ -3,16 +3,25 @@
 import { BlockNoteEditor } from "@blocknote/core";
 import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import "@blocknote/core/style.css";
+import { useEffect, useState } from "react";
 
 interface Props {
   onEditorChange: (blocknote: BlockNoteEditor) => void;
 }
 
 const Editor = ({ onEditorChange }: Props) => {
+  const [client, setClient] = useState(false);
+
+  useEffect(() => {
+    setClient(true);
+  }, []);
+
   const editor: BlockNoteEditor = useBlockNote({
     onEditorContentChange: onEditorChange,
   });
-
+  if (!client) {
+    return null;
+  }
   return <BlockNoteView editor={editor} theme="light" />;
 };
 
