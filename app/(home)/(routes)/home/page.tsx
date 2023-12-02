81c64511b64d1d603076172d6407db7f48270439
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-02 18:20:16 +0530

    add: home page posts display

--- app/(home)/(routes)/home/page.tsx
+++ app/(home)/(routes)/home/page.tsx
@@ -1,7 +1,26 @@
-const HomePage = () => {
+import { PostCard } from "@/components/postcard";
+import { db } from "@/lib/prismadb";
+import { currentUser } from "@clerk/nextjs";
+
+const HomePage = async () => {
+  const user = await currentUser();
+
+  if (!user) return null;
+  const posts = await db.posts.findMany();
+
   return (
-    <div className="p-5">
-      <h1>home page</h1>
+    <div className="p-5 flex flex-col overflow-scroll justify-center items-center w-full">
+      {posts.map((post) => (
+        <PostCard
+          key={post.id}
+          title={post.title}
+          id={post.id}
+          summary={post.summary}
+          createdAt={post.createdAt}
+          imageUrl={user?.imageUrl}
+          username={post.username}
+        />
+      ))}
     </div>
   );
 };
