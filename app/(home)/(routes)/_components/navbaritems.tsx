c2ce18afef0d4b5551e88788d5d73861dc49f8d1
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-06 20:58:55 +0530

    add: changes

--- app/(home)/(routes)/_components/navbaritems.tsx
+++ app/(home)/(routes)/_components/navbaritems.tsx
@@ -15,10 +15,10 @@ export const NavbarItems = ({ imageUrl, name, email }: navbarProps) => {
   const pathname = usePathname();
 
   return (
-    <>
+    <div className="flex items-center gap-3">
       {pathname === "/home" && <CreateButton />}
       {pathname === "/home/create-post" && <PostButton />}
       <UserNavbar name={name} imageUrl={imageUrl} email={email} />
-    </>
+    </div>
   );
 };
