5114f32829dd2398c952836203f461ec0c7fd11c
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-08 18:51:30 +0530

    hero fixes

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 const LandingPage = async () => {
   const { userId } = auth();
   return (
-    <div className="flex justify-between gap-y-4 md:px-32 px-16 py-40 relative">
+    <div className="flex justify-between gap-y-4 md:px-32 px-16 py-36 relative">
       <div className="max-w-3xl space-y-5 flex-col  justify-center">
         <h1 className="font-bold md:text-8xl sm:text-6xl text-7xl pb-1">Stay curious.</h1>
         <h2 className="sm:text-xl md:text-2xl text-xl font-medium">
@@ -34,7 +34,7 @@ const LandingPage = async () => {
           </div>
         )}
       </div>
-      <div className="flex items-start justify-start absolute top-12 right-44">
+      <div className="absolute right-36 top-14">
         <Image src="/hero.svg" height="450" width="450" alt="text" className="hidden lg:block" />
       </div>
     </div>
--- components/logo.tsx
+++ components/logo.tsx
@@ -4,7 +4,7 @@ export const Logo = () => {
   return (
     <div className="flex items-center gap-x-2">
       <Image src="/logo.svg" alt="logo" width={35} height={35} />
-      <p className="text-lg">Wrapped</p>
+      <p className="text-lg font-semibold">Wrapped</p>
     </div>
   );
 };
