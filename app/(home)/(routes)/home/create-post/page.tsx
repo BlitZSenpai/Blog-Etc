8c18f973b61006b83aa627bc343f12eb45af5b57
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-09 22:26:47 +0530

    more mobile fixes

--- app/(home)/(routes)/home/create-post/page.tsx
+++ app/(home)/(routes)/home/create-post/page.tsx
@@ -7,7 +7,7 @@ const CreatePost = async () => {
   if (!user?.username) return null;
 
   return (
-    <div className="p-10 relative md:max-w-3xl lg:mx-w-4xl mx-auto">
+    <div className="md:p-10 p-9 relative md:max-w-3xl lg:mx-w-4xl mx-auto">
       <FormComponent username={user.username} />
     </div>
   );
