30573a661a2cff0fa64a5f352d3f018988f9ddd4
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-15 23:47:41 +0530

    cursor changes

--- app/(home)/(routes)/_components/createbutton.tsx
+++ app/(home)/(routes)/_components/createbutton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export const CreateButton = () => {
   return (
-    <div>
+    <div className="hover:opacity-70">
       <Link href="/home/create-post">
         <Image className="mr-2" src="/create.svg" alt="create" height="35" width="35" />
       </Link>
--- components/logo.tsx
+++ components/logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Logo = () => {
   return (
-    <div className="flex items-center gap-x-2">
+    <div className="flex items-center gap-x-2 cursor-pointer hover:opacity-75">
       <Image src="/logo.svg" alt="logo" width={35} height={35} />
       <p className="text-lg font-semibold">Wrapped</p>
     </div>
