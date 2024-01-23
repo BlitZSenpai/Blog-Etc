ed4db68077998ac2fc56af082455117ff7a4390a
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-01-23 15:05:43 +0530

    add: better navbar

--- app/(home)/(routes)/_components/navbar.tsx
+++ app/(home)/(routes)/_components/navbar.tsx
@@ -6,16 +6,18 @@ import { currentUser } from "@clerk/nextjs";
 export const Navbar = async () => {
   const user = await currentUser();
   return (
-    <div className="w-full flex top-0 bg-background justify-between md:p-6 p-6 md:px-20 px-10 border-b border-slate-200">
-      <Link href="/home">
-        <Logo />
-      </Link>
-      <div className="flex justify-end space-x-3">
-        <NavbarItems
-          imageUrl={user?.imageUrl}
-          email={user?.emailAddresses[0].emailAddress}
-          name={`${user?.username}`}
-        />
+    <div className="top-0 fixed inset-x-0 backdrop-blur-md z-10 border-b border-slate-200">
+      <div className="w-full flex justify-between md:p-6 p-6 md:px-20 px-10 ">
+        <Link href="/home">
+          <Logo />
+        </Link>
+        <div className="flex justify-end space-x-3">
+          <NavbarItems
+            imageUrl={user?.imageUrl}
+            email={user?.emailAddresses[0].emailAddress}
+            name={`${user?.username}`}
+          />
+        </div>
       </div>
     </div>
   );
